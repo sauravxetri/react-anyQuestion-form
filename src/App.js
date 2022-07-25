d8de0d5d73cdae4f4da2a6d30ee815fa87d929e5
sauravxetri <sauravkshetri.121@gmail.com>
2022-07-25 17:52:48 +0545

    Fix some bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Data from "./Data";
 import Question from "./Question";
 
 function App() {
+  const [question,setQuestion] = useState(Data);
   return (
     <main className="container">
       <section className="sidebar">
         <h2>questions and answers about login</h2>
       </section>
       <div>
-        <Question/>
+        {
+          question.map((item) => {
+            return(
+              <Question key ={item.id} {...item}/>
+            )
+          })
+        }
       </div>
     </main>
   );
--- src/Question.js
+++ src/Question.js
@@ -1,33 +1,22 @@
 import React, { useState } from 'react'
-import Data from "./Data";
 import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai";
 
-const Question = () => {
-    const [question,setQuestion] = useState(Data);
+const Question = ({title,info,html_url}) => {
     const [showInfo,setShowInfo] = useState(false);
   return (
-    <article>
-        {
-            question.map((item)=>{
-                return(
-                    <div className='content'>
-                    <header key={item.id} className="title">
-                        <h1>{item.title}</h1>
-                        <button className='btn' onClick={() => setShowInfo(!showInfo)}>{showInfo ? <AiOutlineMinus/>: <AiOutlinePlus/> }</button>
-                    </header>
-                    {showInfo && (<div>
-                        <p>{item.info}</p>
-                        <a href={item.html_url}>Look more about {item.title}</a>
-                        </div>
-                        )
-                    }
-
-                    </div>
-                )
-            })
-
+    <div className='content'>
+        <header className="title">
+            <h1>{title}</h1>
+            <button className='btn' onClick={() => setShowInfo(!showInfo)}> { showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/> }</button>
+        </header>
+        {showInfo && (<div>
+            <p>{info}</p>
+            <a href={html_url}>Look more about {title}</a>
+            </div>
+            )
         }
-    </article>
+
+      </div>
   )
 }
 
